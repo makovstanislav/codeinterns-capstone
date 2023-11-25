699a7fb2529657dc8c7c6cbaa1d0c4bd6115d840
Stan Makov <85253471+makovstanislav@users.noreply.github.com>
2023-11-25 21:16:52 +0100

    Refactor landing page

--- components/dashboard/header/Header.js
+++ components/dashboard/header/Header.js
@@ -32,7 +32,6 @@ function Header() {
             alt="codeinterns"
           ></Image>
         </a>
-
         {/* burger */}
         <svg className={`bi bi-list ${styles.mobTrigger}`} onClick={toggleMobMenu} xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#bdbdbd" viewBox="0 0 16 16">
           <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
